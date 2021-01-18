6519e4daf2e616609402894900423c19214a85fd
Yumeng Sun <yumeng.sun@outlook.com>
2021-01-17 20:56:47 -0500

    reformat table

--- routes/zhoushen.js
+++ routes/zhoushen.js
@@ -5,9 +5,9 @@ let template_table_header = {
     "<>": "tr", "html": [
         {"<>": "th", "html": "序号"},
         {"<>": "th", "html": "歌名"},
+        {"<>": "th", "html": "总收听量"},
         {"<>": "th", "html": "巅峰指数"},
-        {"<>": "th", "html": "收听人数"},
-        {"<>": "th", "html": "总收听量"}
+        {"<>": "th", "html": "收听人数"}
     ]
 }
 
@@ -15,15 +15,13 @@ let template_table_body = {
     "<>": "tr", "html": [
         {"<>": "td", "html": "${index}"},
         {"<>": "td", "html": "${name}"},
+        {"<>": "td", "html": "${listen_count}"},
         {"<>": "td", "html": "${score}"},
-        {"<>": "td", "html": "${listenCnt}"},
-        {"<>": "td", "html": "${listen_count}"}
+        {"<>": "td", "html": "${listenCnt}"}
     ]
 }
 
-function writeHtmlFromScoresJson(data) {
-
-    const date = new Date();
+function writeHtmlFromJson(data) {
     let table_header = json2html.transform(data.details[0], template_table_header);
     let table_body = json2html.transform(data.details, template_table_body);
 
@@ -54,10 +52,10 @@ function writeHtmlFromScoresJson(data) {
                                 </style>
                             </head>`;
     let body = `<h1>周深QQ音乐数据</h1>
-                <p>时间：${date.toLocaleString('zh-CN')}</p>
+                <p>时间：${data.date}</p>
                 <p>粉丝总数：${data.fans}</p>
                 <p>过去24小时总收听人数：${data.totalListenCount}</p><br>
-                <p>累计收听量Top20 (收听人数为过去24小时；总收听量统计方法未知，推测为过去7-10天累计)：</p>
+                <p>累计收听量Top${data.details.length} (收听人数为过去24小时；总收听量统计方法未知，推测为过去7-10天累计)：</p>
                 <table id="my_table"><thead>${table_header}</thead><tbody>${table_body}</tbody></table>`;
     body = `<body>${body}</body>`
 
@@ -66,96 +64,87 @@ function writeHtmlFromScoresJson(data) {
     return html;
 }
 
-module.exports = {
-
-    '/hotsongs': async (req, res) => {
-        const singermid = '003fA5G40k6hKc';
-        const { num = 20, page = 1 } = req.query;
-        const { cache } = global;
-        let cacheKey = `zs_${num}_${page}`;
-        let cacheData = cache.get(cacheKey)
-
-        // get top 20 hot songs
-        const result = await request({
-            url: 'http://u.y.qq.com/cgi-bin/musicu.fcg',
-            data: {
-                data: JSON.stringify({
-                comm: {
-                    ct: 24,
-                    cv: 0
-                },
-                singer: {
-                    method: "get_singer_detail_info",
-                    param: {
+async function _getHitSongs ( { singermid='003fA5G40k6hKc', num=20, page=1 } ) {
+    return request({
+        url: 'http://u.y.qq.com/cgi-bin/musicu.fcg',
+        data: {
+            data: JSON.stringify({
+            comm: {
+                ct: 24,
+                cv: 0
+            },
+            singer: {
+                method: "get_singer_detail_info",
+                module: "music.web_singer_info_svr",
+                param: {
                     sort: 5,
                     singermid,
                     sin:  (page - 1) * num,
                     num,
-                    },
-                    module: "music.web_singer_info_svr"
                 }
-                })
             }
-        });
-    
-        const { songlist: list, extras } = result.singer.data;
-        list.forEach((o, i) => {
-            Object.assign(o, extras[i] || {});
-        });
-
-        const songMidList = list.map( song => {
-            return song.mid;
-        })
-
-        //get hit info
-        const hitInfo = await request({
-            url: 'http://u.y.qq.com/cgi-bin/musicu.fcg',
-            data: {
-                data: JSON.stringify({
+            })
+        }
+    });
+}
+async function _getHitInfo(param) {
+    return request({
+        url: 'http://u.y.qq.com/cgi-bin/musicu.fcg',
+        data: {
+            data: JSON.stringify({
                 comm: {
                     ct: 24,
                     cv: 0
                 },
                 singer: {
                     method: "GetPlayTopData",
-                    param: {
-                    songMidList,
-                    },
-                    module: "music.musicToplist.PlayTopInfoServer"
+                    module: "music.musicToplist.PlayTopInfoServer",
+                    param
                 }
-                })
-            }
-        });
+            })
+        }
+    })
+}
 
-        let totalListenCount = 0;
-        const details = list.map( ( song, index ) => {
-            const picked = (({ mid, name, listen_count }) => ({ mid, name, listen_count }))(song);
-            const { record, score, listenCnt } = hitInfo.singer.data.data[song.mid] || {};
-            picked.record = record ? record.data : undefined;
-            picked.score = score;
-            picked.listenCnt = listenCnt;
-            picked.index = index+1;
-            if( listenCnt ) {
-                let [ count ] = listenCnt.match(/\d+/g);
-                totalListenCount += parseInt( count );
-            }
-            return picked;
-        });
-        cacheData = {
-            result: details.length,
-            data: {
-                totalListenCount: totalListenCount + 'w+',
-                fans: result.singer.data.singer_info.fans,
-                details,
-            }
-        };
-        //res.send(cacheData);
+function _getReportData({ hitSongs, hitInfo }) {
+    let totalListenCount = 0;
+    const { songlist, extras } = hitSongs.singer.data;
+    
+    songlist.forEach((o, i) => {
+        // add total play count into result body
+        Object.assign(o, extras[i] || {});
+    });
+    const details = songlist.map( ( song, index ) => {
+        const picked = (({ mid, name, listen_count }) => ({ mid, name, listen_count }))(song);
+        const { record, score, listenCnt } = hitInfo.singer.data.data[song.mid] || {};
+        picked.record = record ? record.data : undefined;
+        picked.score = score;
+        picked.listenCnt = listenCnt;
+        picked.index = index+1;
+        if( listenCnt ) {
+            let [ count ] = listenCnt.match(/\d+/g);
+            totalListenCount += parseInt( count );
+        }
+        return picked;
+    });
+    data = {
+        date: ( new Date() ).toLocaleString('zh-CN'),
+        totalListenCount: totalListenCount + 'w+',
+        fans: hitSongs.singer.data.singer_info.fans,
+        details,
+    };
+    return data;
+}
+module.exports = {
+
+    '/hitsongs': async (req, res) => {
+        const hitSongs = await _getHitSongs( req.query );
+        const songMidList = hitSongs.singer.data.songlist.map( song => song.mid);
+        const hitInfo = await _getHitInfo({ songMidList });
+        const html = writeHtmlFromJson( _getReportData( { hitSongs, hitInfo } ) );
         res.writeHead(200, {
             'Content-Type': 'text/html'
         });
-
-        let html = writeHtmlFromScoresJson( cacheData.data );
         res.end(html);
-        cache.set(cacheKey, html);
   }
 }
